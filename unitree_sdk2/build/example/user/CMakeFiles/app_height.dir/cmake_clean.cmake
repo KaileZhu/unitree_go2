93ad9cacc7aa0c3df5c070f3ea87a7741660c514
Shuo Zhang <heifolier@163.com>
2025-04-18 09:05:18 +0800

    1

---
+++ unitree_sdk2/build/example/user/CMakeFiles/app_height.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../../bin/app_height"
+  "../../bin/app_height.pdb"
+  "CMakeFiles/app_height.dir/app_height.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app_height.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
